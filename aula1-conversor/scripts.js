b3750fd9e44a65e24a4ab408fa42fed69cf22efb
Elmo Junior <elmojuniorduarte@gmail.com>
2021-09-15 16:54:31 -0300

    aula 2

--- aula1-conversor/scripts.js
+++ aula1-conversor/scripts.js
@@ -35,15 +35,62 @@ function dolar() {
 
   var vlrDolar = inputVlrDolar.value;
 
-  var real = parseInt(vlrDolar) * 5.21;
+  var real = parseFloat(vlrDolar) * 5.21;
 
   var realfixado = real.toFixed(2);
 
-  var pd2 = document.getElementById("pd2");
+  var pd2 = document.getElementById("p1");
   pd2.innerHTML = "R$ " + realfixado;
+  var pd2 = document.getElementById("pd2");
+  pd2.innerHTML = "";
 }
 
-function temp() {
+function libra() {
+  var inputVlrLibra = document.querySelector("#bd1");
+
+  var vlrLibra = inputVlrLibra.value;
+
+  var real = parseFloat(vlrLibra) * 7.27;
+
+  var realfixado = real.toFixed(2);
+
+  var pd2 = document.getElementById("p1");
+  pd2.innerHTML = "R$ " + realfixado;
+  var pd2 = document.getElementById("pd2");
+  pd2.innerHTML = "";
+}
+
+function yenes() {
+  var inputVlrYenes = document.querySelector("#bd1");
+
+  var vlrYenes = inputVlrYenes.value;
+
+  var real = parseFloat(vlrYenes) * 0.03162;
+
+  var realfixado = real.toFixed(2);
+
+  var pd2 = document.getElementById("p1");
+  pd2.innerHTML = "R$ " + realfixado;
+  var pd2 = document.getElementById("pd2");
+  pd2.innerHTML = "";
+}
+
+function wons() {
+  var inputVlrWons = document.querySelector("#bd1");
+
+  var vlrWons = inputVlrWons.value;
+
+  var real = parseFloat(vlrWons) * 0.0045;
+
+  var realfixado = real.toFixed(2);
+
+  var pd2 = document.getElementById("p1");
+  pd2.innerHTML = "R$ " + realfixado;
+  var pd2 = document.getElementById("pd2");
+  pd2.innerHTML = "";
+}
+
+function CF() {
   var inputCTemp = document.querySelector("#bt1");
 
   var cTemp = inputCTemp.value;
@@ -52,6 +99,53 @@ function temp() {
 
   var fTempFixada = fTemp.toFixed(1);
 
-  var pt2 = document.getElementById("pt2");
+  var pt2 = document.getElementById("p1");
   pt2.innerHTML = fTempFixada + "ºF";
+  var pd2 = document.getElementById("pt2");
+  pd2.innerHTML = "";
+}
+
+function CK() {
+  var inputCTemp = document.querySelector("#bt1");
+
+  var cTemp = inputCTemp.value;
+
+  var kTemp = parseInt(cTemp) + 273;
+
+  var kTempFixada = kTemp.toFixed(1);
+
+  var pt2 = document.getElementById("p1");
+  pt2.innerHTML = kTempFixada + "ºK";
+  var pd2 = document.getElementById("pt2");
+  pd2.innerHTML = "";
+}
+
+function kmToAl() {
+  var inputKm = document.querySelector("#bt1");
+
+  var Km = inputKm.value;
+
+  var al = parseFloat(Km) / 9.461e12;
+
+  // var alFixada = al.toFixed(1);
+
+  var pt2 = document.getElementById("p1");
+  pt2.innerHTML = al + " Ano(s) luz";
+  var pd2 = document.getElementById("pt2");
+  pd2.innerHTML = "";
+}
+
+function alToKm() {
+  var inputAl = document.querySelector("#bt2");
+
+  var als = inputAl.value;
+
+  var kms = parseFloat(als) * 9.461e12;
+
+  // var kmFixada = km.toFixed(1);
+
+  var pt2 = document.getElementById("p1");
+  pt2.innerHTML = kms + " Kilometros";
+  var pd2 = document.getElementById("pt2");
+  pd2.innerHTML = "";
 }
