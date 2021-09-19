96021fd25ad639a5f5331e6e2ee72f6ab0db1b77
Elmo Junior <elmojuniorduarte@gmail.com>
2021-09-19 02:48:31 -0300

    aula 4 v2

--- aula4-AluraFlix/script.js
+++ aula4-AluraFlix/script.js
@@ -1,32 +1,96 @@
 var filmes = [
   "https://i.ytimg.com/vi/-1gL374FIdc/movieposter.jpg",
   "https://static.wikia.nocookie.net/netflix/images/9/99/Navillera0.jpg",
-  "https://br.web.img2.acsta.net/pictures/21/04/07/21/13/5564614.jpg",
   "https://upload.wikimedia.org/wikipedia/pt/2/29/Pitch_Perfect.jpg",
   "https://ingresso-a.akamaihd.net/prd/img/movie/demon-slayer-kimetsu-no-yaiba-the-movie-infinity-train/505c829b-023d-406a-b6ae-b6c84cb9bde1.jpg",
 ];
 
+var nomeFilme = [
+  "Steven Universe: The Movie",
+  "Navillera",
+  "A Escolha Perfeita",
+  "Demon Slayer: Kimetsu no Yaiba",
+];
+
 var section = document.getElementById("secfilmes");
 
-for (let i = 0; i < filmes.length; i++) {
-  section.innerHTML += "<img src=" + filmes[i] + " >";
-}
+imprimirTela();
 
 function adicionar() {
   var urlimg = document.getElementById("url").value;
+  var nome = document.getElementById("nome").value;
 
-  if (filmes.includes(urlimg)) {
-    var msgja = document.getElementById("msg");
-    msgja.innerHTML = "Essa imagem já foi adicionada.";
+  if (urlimg.endsWith(".jpg") || urlimg.endsWith(".png")) {
+    if (filmes.includes(urlimg)) {
+      var msgja = document.getElementById("msg");
+      msgja.innerHTML = "Essa imagem já foi adicionada.";
+
+      limparinput();
+    } else {
+      if (nome.length > 0) {
+        limparMsgErro();
+
+        filmes.unshift(urlimg);
+        nomeFilme.unshift(nome);
+
+        imprimirTela();
+      } else {
+        var msgja = document.getElementById("msg");
+        msgja.innerHTML = "Titulo não pode estar vazio";
+      }
+    }
   } else {
     var msgja = document.getElementById("msg");
-    msgja.innerHTML = "";
-    filmes.unshift(urlimg);
-
-    var section = document.getElementById("secfilmes");
-    section.innerHTML = "";
-    for (let i = 0; i < filmes.length; i++) {
-      section.innerHTML += "<img src=" + filmes[i] + " >";
-    }
+    msgja.innerHTML = " Endereço de imagem inválida ;-; ";
+    limparinput();
   }
+}
+
+function imprimirTela() {
+  var section = document.getElementById("secfilmes");
+  section.innerHTML = "";
+  for (let i = 0; i < filmes.length; i++) {
+    section.innerHTML +=
+      "<div class='divfilmes'><img src=" +
+      filmes[i] +
+      " ><p>" +
+      nomeFilme[i] +
+      "</p></div";
+    limparinput();
+  }
+}
+
+function remover() {
+  var tituloRemover = document.getElementById("nomer").value;
+  console.log(tituloRemover);
+  if (nomeFilme.includes(tituloRemover)) {
+    if (tituloRemover.length > 0) {
+      var i = nomeFilme.indexOf(tituloRemover);
+
+      console.log(i);
+
+      filmes.splice(i, 1);
+      nomeFilme.splice(i, 1);
+
+      imprimirTela();
+      limparMsgErro();
+      limparinput();
+    }
+  } else {
+    var msgja = document.getElementById("msgr");
+    msgja.innerHTML = "Titulo não encontrado";
+  }
+}
+
+function limparMsgErro() {
+  var msgja = document.getElementById("msgr");
+  msgja.innerHTML = "";
+  var msgja = document.getElementById("msg");
+  msgja.innerHTML = "";
+}
+
+function limparinput() {
+  document.getElementById("url").value = "";
+  document.getElementById("nome").value = "";
+  document.getElementById("nomer").value = "";
 }
